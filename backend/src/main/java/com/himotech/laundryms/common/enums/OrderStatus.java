9392407b4e2889b8bc5c7041613609a0178e18f3
Mark Alvin Cadangin <markalvin.cadangin@wvsu.edu.ph>
2026-02-08 01:46:03 +0800

    feat(enums): add user roles, payment methods, statuses, and order statuses

---
+++ backend/src/main/java/com/himotech/laundryms/common/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.himotech.laundryms.common.enums;
+
+public enum OrderStatus {
+    RECEIVED,
+    WASHING,
+    DRYING,
+    FOLDING,
+    READY_FOR_PICKUP,
+    RELEASED,
+    CANCELLED
+}
---
+++ backend/src/main/java/com/himotech/laundryms/common/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.himotech.laundryms.common.enums;
+
+public enum UserRole {
+    OWNER,
+    STAFF
+}
